c13ecb2b446a2da3f4c1d9d921767fdbe4e804bd
Võ Anh Nhật <nhatva7980@ut.edu.vn>
2025-05-08 00:32:56 +0700

    Add files via upload

---
+++ delete_form_image.py
@@ -0,0 +1,7 @@
+﻿from rembg import remove
+from PIL import Image
+input_path = r"C:\MEARCHING LEARNING\test\nhom.jpg"
+output_path = r"C:\MEARCHING LEARNING\test\nhom.png"
+inp = Image.open(input_path)
+output_path = remove(inp)
+Image.open("nhom.path")
